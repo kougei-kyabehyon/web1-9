fc4f762f17ea446aee00fd5d5cf0e0a1df541992
Unknown <sorldart0313@i.softbank.jp>
2018-07-09 18:26:23 +0900

    basukegasitaidesu

---
+++ AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+
+
+    GameObject timerText;
+    GameDirector pointText;
+    float time = 60.0f;
+    int point = 0;
+
+    public void GetApple()
+    {
+        this.point += 100;
+    }
+
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.timerText = GameDirector.Find("Time");
+        this.pointText = GameObject.Find("Point");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + "point";
+
+    }
+}
---
+++ AppleCatch/Assets/ItemGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    float span = 1.0f;
+    float delta = 0;
+    int ratio;
+    float speed = -0.03f;
+	// Use this for initialization
+    public void SetParameter(float span,float speed,int ratio)
+    {
+        this.span = span;
+        this.speed = speed;
+        this.ratio;
+    }
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject item;
+            int dice = Random.Range(1, 11);
+            if (dice <= this.span)
+            {
+                item = Instantiate(bombPrefab) as GameObject;
+
+            }
+            else
+            {
+                item = Instantiate(applePrefab) as GameObject;
+            }
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            itcm.transform.positon = new Vector(x, 4, z);
+
+            item.GetComponent<ItemGenerator>().dropSpeed = this.speed;
+        }
+    }
+}
